0fc1bc360d451da9b8f0a78c7bd0240c721893da
Marcin Kaciuba <marcin.kaciuba@gmail.com>
2025-12-16 16:50:31 +0100

    bump

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,9 @@
-FROM debian:stretch
-RUN apt-get update && apt-get install python python-yaml -y && apt-get install ruby ruby-dev rubygems build-essential sudo git wget tar unzip -y && ldconfig
-RUN gem install --no-ri --no-rdoc fpm
+FROM debian:trixie
+RUN apt-get update && apt-get install python3 python3-yaml python-is-python3 -y && apt-get install ruby ruby-dev build-essential sudo git wget tar unzip -y && ldconfig
+RUN gem install --no-document fpm
 WORKDIR /workspace
 ADD . /workspace
 RUN python compile.py
 RUN ls /tmp/nginx-custom
-ENV VERSION-$(cat /workspace/modules.yml | grep version | head -n 1 | awk  '{ gsub("\"", ""); print $2 }')
 RUN cd /workspace; VERSION=$(cat /workspace/modules.yml | grep version | head -n 1 | awk  '{ gsub("\"", ""); print $2 }'); bash /workspace/debpackage.sh /tmp/nginx-custom/nginx/nginx-${VERSION}  ${VERSION}; mkdir -p /workspace/result; cp /workspace/nginx-extras-amd64-${VERSION}.deb /workspace/result
 ENTRYPOINT ["cp" , "-r", "/workspace/result", "/result/"]
--- compile.py
+++ compile.py
@@ -3,10 +3,10 @@ import yaml
 import os
 import subprocess
 
-config = yaml.load(open('./modules.yml'))
+config = yaml.load(open('./modules.yml'), Loader=yaml.FullLoader)
 hooks = config['hooks']
 env = os.environ
-for key, value in config['environment'].iteritems():
+for key, value in config['environment'].items():
     if value:
         env[key] = value
 
@@ -41,7 +41,7 @@ def download(url, path):
 
 def prepare_modules(modules):
     modules_path = []
-    for name, mod in modules.iteritems():
+    for name, mod in modules.items():
         path = os.path.join(base_path, name)
         if mod['source'].endswith('.git'):
             clone_repo(mod['source'], path, mod.get('version'), mod.get('recursive', False))
@@ -72,7 +72,7 @@ configure_args = configure(config['configure'], modules)
 
 nginx_path = download('http://nginx.org/download/nginx-' + config['version'] + '.tar.gz', os.path.join(base_path, 'nginx'))
 
-print configure_args
+print(configure_args)
 
 shell('cd ' + nginx_path + ' && ./configure ' + configure_args)
 if 'postconfigure' in hooks:
